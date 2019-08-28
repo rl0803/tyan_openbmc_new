198e7d5d4b369bbef63d7598c874c3b0d6184618
Frederick Lee <Frederick_Lee@wiwynn.com>
2019-08-28 16:09:44 +0800

    [sv300g3e] Add to fix a host power on-off-on behavior during a host power-on process. - Disable the host-on watchdog timer when the pgood state turns from low to high.
    Test:
    1. Perform host power on/off/cycle with IPMI CMD (ipmitool power on/off/cycle).
    2. Check the power status with IPMI CMD (ipmitool power status).
    3. Confirm that the power on-off-on issue is fixed.

--- meta-wiwynn/meta-sv300g3e/recipes-phosphor/sv300g3e-powerctrl/files/poweron.sh
+++ meta-wiwynn/meta-sv300g3e/recipes-phosphor/sv300g3e-powerctrl/files/poweron.sh
@@ -5,8 +5,25 @@ echo "[SV300G3-E][S] System Power on"
 pwrstatus=$(/usr/bin/gpioget gpiochip0 101)
 if [ ${pwrstatus} -eq 0 ]; then
     /usr/bin/gpioset gpiochip0 27=0
-    sleep 2
-    /usr/bin/gpioset gpiochip0 27=1 
+    sleep 1
+    /usr/bin/gpioset gpiochip0 27=1
+    sleep 1
+    # Monitor the PGood Status
+    CHECK=0
+    while [ ${CHECK} -lt 10 ]
+    do
+        sleep 1
+        pwrstatus=$(/usr/bin/gpioget gpiochip0 101)
+        if [ ${pwrstatus} -eq 1 ]; then
+            busctl set-property xyz.openbmc_project.Watchdog /xyz/openbmc_project/watchdog/host0 xyz.openbmc_project.State.Watchdog Enabled b false
+            if [ $? == 0 ]; then
+                break;
+            fi
+        fi
+        echo "${CHECK} - Failed to disable WDT."
+        (( CHECK=CHECK+1 ))
+    done
+
 fi
 
 echo "[SV300G3-E][P] System Power on"
