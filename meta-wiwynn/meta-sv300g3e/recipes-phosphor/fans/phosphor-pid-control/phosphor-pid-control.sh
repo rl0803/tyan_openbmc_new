73c28ec9e168d44e5e793e50bf2aab32373bb932
Frederick Lee <Frederick_Lee@wiwynn.com>
2020-06-16 23:25:51 +0800

    [sv300g3e] Set the fan PWM to 80 percent when the host power state changes from off to on - Add a dbus property, postcomplete, to indicate the bios post status. - Set the fan PWM to 80% until an end-of-post event occurs.
    Tested:
    Checked the fan speed vary during the host power-on/initiation. - Pass
    -------------------------------------------------------
    1. The host power state changes from off to on.
    1.1 Host power state transition:
    root@sv300g3e:~# ipmitool power status
    Chassis Power is off
    root@sv300g3e:~# ipmitool power on
    Chassis Power Control: Up/On
    root@sv300g3e:~# ipmitool power status
    Chassis Power is on
    
    1.2 FAN PWM to 80%:
    root@sv300g3e:~# ipmitool sdr | grep FAN
    SYS FAN0         | 13416 RPM         | ok
    SYS FAN1         | 13416 RPM         | ok
    SYS FAN2         | 13416 RPM         | ok
    SYS FAN3         | 13416 RPM         | ok
    SYS FAN4         | 13674 RPM         | ok
    SYS FAN5         | 13502 RPM         | ok
    
    2. After an end-of-post event occurs.
    2.1 SEL entries:
    root@sv300g3e:~# ipmitool sel elist
       1 | 06/16/20 | 14:45:31 UTC | Event Logging Disabled EventLogging | Log area reset/cleared | Asserted
       2 | 06/16/20 | 14:58:28 UTC | System ACPI Power State ACPI Power State | S5/G2: soft-off | Asserted
       3 | 06/16/20 | 15:00:16 UTC | System ACPI Power State ACPI Power State | S0/G0: working | Asserted
       4 | 06/16/20 | 15:02:21 UTC | System Event End of Post | OEM System boot event | Asserted
    
    2.2 FAN PWM to follow FSC algorithm:
    root@sv300g3e:~# ipmitool sdr | grep FAN
    SYS FAN0         | 3010 RPM          | ok
    SYS FAN1         | 2838 RPM          | ok
    SYS FAN2         | 3010 RPM          | ok
    SYS FAN3         | 2924 RPM          | ok
    SYS FAN4         | 3010 RPM          | ok
    SYS FAN5         | 2924 RPM          | ok

--- meta-wiwynn/meta-sv300g3e/recipes-phosphor/fans/phosphor-pid-control/phosphor-pid-control.sh
+++ meta-wiwynn/meta-sv300g3e/recipes-phosphor/fans/phosphor-pid-control/phosphor-pid-control.sh
@@ -3,6 +3,15 @@
 fanTablePath="/run/initramfs/ro/usr/share/entity-manager/configurations/fan-table.json"
 
 if [ -f "$fanTablePath" ]; then
+    # Check the post complete property status
+    pcstatus=$(busctl get-property org.openbmc.control.Power /org/openbmc/control/power0 org.openbmc.control.PostComplete postcomplete | cut -d ' ' -f 2)
+    while [ ${pcstatus} -ne 0 ]
+    do
+        /usr/bin/writePwm.sh 80
+        sleep 5
+        pcstatus=$(busctl get-property org.openbmc.control.Power /org/openbmc/control/power0 org.openbmc.control.PostComplete postcomplete | cut -d ' ' -f 2)
+    done
+    sleep 1
     /usr/bin/swampd -c $fanTablePath
 else
     # If fan table do not exist, write 100% pwm and stop phosphor-pid-control.service
--- meta-wiwynn/meta-sv300g3e/recipes-phosphor/gpio/sv300g3e-gpio-monitor-register/end-of-post-event.sh
+++ meta-wiwynn/meta-sv300g3e/recipes-phosphor/gpio/sv300g3e-gpio-monitor-register/end-of-post-event.sh
@@ -8,4 +8,12 @@ METHOD="IpmiSelAdd"
 
 busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "SEL Entry" "/xyz/openbmc_project/sensors/systemevent/End_of_Post" 3 {0x1,0xff,0xff} yes 0x20
 
+# Set the "postcomplete" property to "0"
+SERVICE_PC="org.openbmc.control.Power"
+OBJECT_PC="/org/openbmc/control/power0"
+INTERFACE_PC="org.openbmc.control.PostComplete"
+PROPERTY="postcomplete"
+
+busctl set-property $SERVICE_PC $OBJECT_PC $INTERFACE_PC $PROPERTY i 0
+
 exit 0
