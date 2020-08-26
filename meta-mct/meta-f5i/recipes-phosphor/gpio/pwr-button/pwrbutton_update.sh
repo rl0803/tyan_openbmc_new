8b5f4fd10ea70942f6c35ebbf46f7c8983fa1046
John Chung <john.chung@mic.com.tw>
2020-08-26 17:07:54 +0800

    [f5i] Fix the wrong sensor name for reset button event
    Summary: Fix the wrong sensor name for reset button event.
    
    Test Plan:
    Check the button event works normally via press the button by manual.
    
    root@f5i:~# ipmitool  sel elist
       1 | 10/21/20 | 15:30:43 UTC | Event Logging Disabled EVENT CLEARED | Log area reset/cleared | Asserted
       2 | 10/21/20 | 15:30:47 UTC | Button BUTTON | Reset Button pressed | Asserted
       3 | 10/21/20 | 15:30:47 UTC | Button BUTTON | Power Button pressed | Asserted
    
    ->Pass
    
    Reviewers: ray.lue, wendy.shi
    
    Reviewed By: ray.lue
    
    Differential Revision: http://10.99.241.45/D1448

--- meta-mct/meta-f5i/recipes-phosphor/gpio/pwr-button/pwrbutton_update.sh
+++ meta-mct/meta-f5i/recipes-phosphor/gpio/pwr-button/pwrbutton_update.sh
@@ -5,4 +5,4 @@ OBJECT="/xyz/openbmc_project/Logging/IPMI"
 INTERFACE="xyz.openbmc_project.Logging.IPMI"
 METHOD="IpmiSelAdd"
 
-busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "SEL Entry" "/xyz/openbmc_project/sensors/pwr_button/POWER_BUTTON" 3 {0x00,0x01,0x00} yes 0x20
+busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "SEL Entry" "/xyz/openbmc_project/sensors/pwr_button/BUTTON" 3 {0x00,0x01,0x00} yes 0x20
