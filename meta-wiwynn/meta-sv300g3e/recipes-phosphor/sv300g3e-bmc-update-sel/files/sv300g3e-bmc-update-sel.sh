16a17722713469e87315b8c05e5e7172532f2fc8
Frederick Lee <Frederick_Lee@wiwynn.com>
2020-09-29 18:39:53 +0800

    [sv300g3e] Support BMC FW version change SEL for in-band update method - The BMC FW version change SEL will be recorded automatically after flashing BMC FW with in-band method. - Sensor name: IB BMC FW UPDATE - Sensor number: 0xFE
    Tested:
    Verified the BMC version change SEL entry by updating the BMC FW via in-band and OOB. - Pass
    ------------------------------------------------------------------------------
    In-band:
    root@sv300g3e:~# ipmitool sel elist | grep -i "UPDATE"
       2 | 09/29/20 | 09:13:56 UTC | Version Change IB BMC FW UPDATE |  | Asserted
    root@sv300g3e:~# ipmitool sel get 0x2
    SEL Record ID          : 0002
     Record Type           : 02
     Timestamp             : 09:13:56 UTC 09:13:56 UTC
     Generator ID          : 0020
     EvM Revision          : 04
     Sensor Type           : Version Change
     Sensor Number         : fe
     Event Type            : Sensor-specific Discrete
     Event Direction       : Assertion Event
     Event Data (RAW)      : 070000
     Event Interpretation  : Missing
     Description           :
    
    Sensor ID              : IB BMC FW UPDATE (0xfe)
    Entity ID              : 0.1 (Unspecified)
    Sensor Type            : Version Change (0x2b)
    
    OOB:
    root@sv300g3e:~# ipmitool sel elist | grep -i "UPDATE"
       2 | 09/29/20 | 09:44:09 UTC | Version Change BMC FW UPDATE |  | Asserted
    root@sv300g3e:~# ipmitool sel get 0x2
    SEL Record ID          : 0002
     Record Type           : 02
     Timestamp             : 09:44:09 UTC 09:44:09 UTC
     Generator ID          : 0020
     EvM Revision          : 04
     Sensor Type           : Version Change
     Sensor Number         : 56
     Event Type            : Sensor-specific Discrete
     Event Direction       : Assertion Event
     Event Data (RAW)      : 070000
     Event Interpretation  : Missing
     Description           :
    
    Sensor ID              : BMC FW UPDATE (0x56)
    Entity ID              : 0.1 (Unspecified)
    Sensor Type            : Version Change (0x2b)

--- meta-wiwynn/meta-sv300g3e/recipes-phosphor/sv300g3e-bmc-update-sel/files/sv300g3e-bmc-update-sel.sh
+++ meta-wiwynn/meta-sv300g3e/recipes-phosphor/sv300g3e-bmc-update-sel/files/sv300g3e-bmc-update-sel.sh
@@ -4,15 +4,24 @@ SERVICE="xyz.openbmc_project.Logging.IPMI"
 OBJECT="/xyz/openbmc_project/Logging/IPMI"
 INTERFACE="xyz.openbmc_project.Logging.IPMI"
 METHOD="IpmiSelAdd"
+METHOD_OEM="IpmiSelAddOem"
 
 /sbin/fw_printenv | grep 'bmc_update'
 res=$?
 
-if [ $res -eq 0 ]; then
+if [[ ${res} -eq 0 ]]; then
     busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "SEL Entry" "/xyz/openbmc_project/sensors/versionchange/BMC_FW_UPDATE" 3 {0x07,0x00,0x00} yes 0x20
     /sbin/fw_setenv bmc_update
+    /sbin/fw_setenv update_sel_add yes
     exit 0;
 fi
 
-exit 0;
+/sbin/fw_printenv | grep 'update_sel_add'
+res=$?
 
+if [[ ${res} -eq 1 ]]; then
+    busctl call ${SERVICE} ${OBJECT} ${INTERFACE} ${METHOD_OEM} sayy "SEL Entry" 9 {0x20,0x0,0x4,0x2b,0xfe,0x6f,0x7,0x0,0x0} 0x02
+    /sbin/fw_setenv update_sel_add yes
+fi
+
+exit 0;
