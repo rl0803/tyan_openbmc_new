51041fb50c2170d6510022378deb8bc1efaaf94e
wendy.shi <wendy.shi@mic.com.tw>
2020-10-16 16:52:07 +0800

    [f5i] Add NIC card FRU device
    Summary:
    1. support Intel-V710 NIC card FRU device
    2. modify Intel-V710 NIC card temp sensor name
     - from "NIC1_Temp" to "NIC_Temp"
    
    Test Plan:
    1. check Intel-V710 NIC card , fru data and sensor reading -> PASS
    
    NIC Temp         | 42.000     | degrees C  | ok    | na        | na        | na        | na        | 105.000   | na
    
    FRU Device Description : _Intel XL710 (ID 70)
     Board Mfg Date        : Thu May 11 12:00:00 2017 UTC
     Board Mfg             : INTE
     Board Product         : :7496,74656.20584.373130
     Board Serial          : 4441314_43502041647074. 4 32373630
     Board Part Number     : 2d303036c6393432373532c830382f31372f313603d2303000d54852289009c10000000000000000002900000050f08e0008b08e
    
    2. check sensor reading and fru data when insert/not_insert NIC card
     - AC cycle -> PASS
     - DC cycle -> PASS
    
    Reviewers: ray.lue, john.chung
    
    Reviewed By: john.chung
    
    Differential Revision: http://10.99.241.45/D1476

--- meta-mct/meta-f5i/recipes-phosphor/s7106-default-action/files/disable-sw2.sh
+++ meta-mct/meta-f5i/recipes-phosphor/s7106-default-action/files/disable-sw2.sh
@@ -11,16 +11,9 @@ while [ $BMC_state != "Ready" ]; do
     sleep 5
 done
 
-service="xyz.openbmc_project.HwmonTempSensor"
-object="/xyz/openbmc_project/sensors/temperature/NIC_Temp"
-interface="xyz.openbmc_project.Sensor.Value"
-property="Value"
-sensor_value=$(busctl get-property $service $object $interface $property | awk -F' ' '{print $2}')
-
-if [ -z "$sensor_value" ]; then
-    #/usr/bin/gpioset gpiochip0 49=1
-    echo 0x1f > /sys/bus/i2c/devices/i2c-14/delete_device
+if [ ! -d "/sys/bus/i2c/devices/14-001f/hwmon" ]; then
     echo " Didn't have switch port1 OCP card"
+    echo 0x1f > /sys/bus/i2c/devices/i2c-14/delete_device
 
     pservice="xyz.openbmc_project.State.Chassis"
     pinterface="xyz.openbmc_project.State.Chassis"
@@ -28,25 +21,29 @@ if [ -z "$sensor_value" ]; then
     pproperty="CurrentPowerState"
     Power_state="Off"
 
+    # check switch port4 OCP card (Intel-V710) when system power on
     while [ $Power_state != "On" ]; do
         Power_state=$(busctl get-property $pservice $pobject $pinterface $pproperty | awk -F'"' '{print $2}' | awk -F'.' '{print $6}')
         sleep 5
     done
 
     if [ ! -d "/sys/bus/i2c/devices/17-001f/hwmon" ]; then
-        echo " Dir .../17-001f/hwmon does not exists , create port4 OCP card "
+        echo " Dir .../17-001f/hwmon does not exists , Re-scan port4 OCP card "
         echo 0x1f > /sys/bus/i2c/devices/i2c-17/delete_device
         echo tmp75 0x1f > /sys/bus/i2c/devices/i2c-17/new_device
-        systemctl restart xyz.openbmc_project.hwmontempsensor.service
 
         if [ ! -d "/sys/bus/i2c/devices/17-001f/hwmon" ]; then
+            echo 0x51 > /sys/bus/i2c/devices/i2c-17/delete_device
             echo 0x1f > /sys/bus/i2c/devices/i2c-17/delete_device
             /usr/bin/gpioset gpiochip0 49=1
             echo " Both OCP card does not exists , disable SW2 "
+        else
+            echo " switch port4 OCP card exists , re-scan FruDevice"
+            systemctl restart xyz.openbmc_project.FruDevice.service
         fi
     fi
 else
+    echo 0x51 > /sys/bus/i2c/devices/i2c-17/delete_device
     echo 0x1f > /sys/bus/i2c/devices/i2c-17/delete_device
     echo "Didn't have switch port4 OCP card"
 fi
-
