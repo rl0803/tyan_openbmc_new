185d1f65c127442722542e4fbbc4c4cb0bbae45a
John Chung <john.chung@mic.com.tw>
2021-03-09 15:48:33 +0800

    [f5i] Update AMT sensor name for identified AMT started and completed
    Summary:
    Change AMT sensor name to AMT_START and AMT_COMPLETE.
    Using different sensor name to identify AMT process started or completed.
    
    Test Plan:
    Check AMT SEL feature works normally.
    
    root@f5i:~# ipmitool sel elist |grep -i amt
       d | 03/08/21 | 21:44:54 UTC | System Event AMT START | System Reconfigured | Asserted
       e | 03/08/21 | 21:53:30 UTC | System Event AMT COMPLETE | System Reconfigured | Asserted
    
    ->Pass
    
    Reviewers: wendy.shi, ray.lue
    
    Reviewed By: ray.lue
    
    Differential Revision: http://10.99.241.45/D1543

--- meta-mct/meta-f5i/recipes-phosphor/gpio/phosphor-gpio-monitor/setAmtEvent.sh
+++ meta-mct/meta-f5i/recipes-phosphor/gpio/phosphor-gpio-monitor/setAmtEvent.sh
@@ -9,7 +9,7 @@ INTERFACE="xyz.openbmc_project.Logging.IPMI"
 METHOD="IpmiSelAdd"
 
 if [ "$type" == "assert" ]; then
-   busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "AMT SEL Entry" "/xyz/openbmc_project/sensors/system_event/AMT" 3 {0x00,0x00,0xff} yes 0x20  
+   busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "AMT SEL Entry" "/xyz/openbmc_project/sensors/system_event/AMT_START" 3 {0x00,0x00,0xff} yes 0x20
 else
-   busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "AMT SEL Entry" "/xyz/openbmc_project/sensors/system_event/AMT" 3 {0x00,0x00,0xff} no 0x20
+   busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "AMT SEL Entry" "/xyz/openbmc_project/sensors/system_event/AMT_COMPLETE" 3 {0x00,0x00,0xff} yes 0x20
 fi
