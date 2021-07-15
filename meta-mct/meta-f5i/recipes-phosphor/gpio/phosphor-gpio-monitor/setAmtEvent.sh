0e14692e14f0abfe8b9c6e8cfecc2c1dfac5bff4
John Chung <john.chung@mic.com.tw>
2021-07-15 10:10:14 +0800

    [f5i] Filter out abnormal AMT SEL event which cause by host reset
    Summary:
    Some system would generate abnormal AMT SEL cause AMT GPIO trigger by host reset.
    It's fake AMT SEL.
    Using firmware to workaround for this issue.
    
    Test Plan:
    Check function works normally.
    AC cycle for 40 times and make sure it does not have abnormal AMT SEL.
    
    root@f5i:~# ipmitool sel elist |grep -i AMT
       6 | 06/25/21 | 15:12:14 UTC | System Event AMT START | System Reconfigured | Asserted
       7 | 06/25/21 | 15:15:03 UTC | System Event AMT COMPLETE | System Reconfigured | Asserted
    root@f5i:~# ipmitool sel elist |egrep -c "SYSTEM RESTART"
    40
    
    ->Pass
    
    Reviewers: ray.lue, wendy.shi
    
    Reviewed By: wendy.shi
    
    Differential Revision: https://phabricator1.mic.com.tw/D1246

--- meta-mct/meta-f5i/recipes-phosphor/gpio/phosphor-gpio-monitor/setAmtEvent.sh
+++ meta-mct/meta-f5i/recipes-phosphor/gpio/phosphor-gpio-monitor/setAmtEvent.sh
@@ -8,8 +8,25 @@ OBJECT="/xyz/openbmc_project/Logging/IPMI"
 INTERFACE="xyz.openbmc_project.Logging.IPMI"
 METHOD="IpmiSelAdd"
 
+AMT_SERVICE="xyz.openbmc_project.Settings"
+AMT_OBJECT="/xyz/openbmc_project/oem/HostStatus"
+AMT_INTERFACE="xyz.openbmc_project.OEM.HostStatus"
+AMT_PROPERTY="AmtMonitorStatus"
+
+AMT_FILE="/run/AMT_START"
+
+AmtMonitorStatus=$(busctl get-property $AMT_SERVICE $AMT_OBJECT $AMT_INTERFACE $AMT_PROPERTY |awk 'NR==1 {print $2}')
+
+if [ "$AmtMonitorStatus" == "false" ]; then
+   exit 0
+fi
+
 if [ "$type" == "assert" ]; then
    busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "AMT SEL Entry" "/xyz/openbmc_project/sensors/system_event/AMT_START" 3 {0x00,0x00,0xff} yes 0x20
+   touch $AMT_FILE
 else
-   busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "AMT SEL Entry" "/xyz/openbmc_project/sensors/system_event/AMT_COMPLETE" 3 {0x00,0x00,0xff} yes 0x20
+   if [ -f $AMT_FILE ]; then
+      busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "AMT SEL Entry" "/xyz/openbmc_project/sensors/system_event/AMT_COMPLETE" 3 {0x00,0x00,0xff} yes 0x20
+      rm -f $AMT_FILE
+   fi
 fi
--- meta-mct/meta-f5i/recipes-phosphor/state/phosphor-post-code-manager_%.bbappend
+++ meta-mct/meta-f5i/recipes-phosphor/state/phosphor-post-code-manager_%.bbappend
@@ -2,5 +2,6 @@ FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 
 SRC_URI += "file://0001-Implement-the-clear-bios-post-code-feature-for-host-.patch \
             file://0002-Print-out-current-boot-cycle-index-to-journal-log-fo.patch \
+            file://0003-Set-AMT-monitoring-functino-status-via-specified-pos.patch \
            "
 
